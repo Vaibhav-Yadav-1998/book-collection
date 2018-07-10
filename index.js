a218f73a09fc9e47f795185c8baf4e383faa2da8
Vaibhav Yadav <vaibhavy131@gmail.com>
2018-07-11 00:10:16 +0530

    added cors for api req

--- index.js
+++ index.js
@@ -2,18 +2,36 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const router = require("./Routes/router");
+const cors = require("cors");
+const Config = require("./config.json");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // mongodb database URI
-const dbURI = process.env.DBURI;
+const dbURI = Config.dbURI;
 
 // connecting to mongodb database
-mongoose.connect(
-  dbURI,
-  { useNewUrlParser: true }
-);
+mongoose
+  .connect(
+    dbURI,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("connected to database"))
+  .catch(err => console.log(err));
+
+// whitelist for cors
+const whitelist = Config.whitelist;
+// cors config
+const corsOption = {
+  origin: function(origin, callback) {
+    whitelist.indexOf(origin) !== -1
+      ? callback(null, true)
+      : callback(new Error("Not Allowed by CORS"));
+  }
+};
+// enabling cors for all routes
+app.use(cors(corsOption));
 
 // use body-parser middleware
 app.use(bodyParser.json());
